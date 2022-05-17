343f3eb1a9891f0066ef7dd41a80a491aec4f36f
GabeKebab <andremmvgabriel@gmail.com>
2022-05-16 17:51:51 -0700

    Removed unused formatter.

--- src/gabe/logging/formatters/Severity.cpp
+++
@@ -1,7 +0,0 @@
-#include <gabe/logging/formatters/Severity.hpp>
-
-gabe::logging::formatters::Severity::Severity() : Formatter::Formatter("SeverityFormatter", "%sev") {}
-
-std::string gabe::logging::formatters::Severity::_format() {
-    return "";
-}

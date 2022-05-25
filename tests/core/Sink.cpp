22fa500f38dc4ba8e5038064bc4536fc935d1bb3
GabeKebab <andremmvgabriel@gmail.com>
2022-05-25 05:32:11 -0700

    Fixed Sink unit tests.

--- tests/core/Sink.cpp
+++ tests/core/Sink.cpp
@@ -8,16 +8,35 @@ class SinkMocker : public gabe::logging::core::Sink
 {
 public:
     using Sink::_buffer;
-    using Sink::_buffer_pos;
+
+    SinkMocker() : Sink() {}
+    SinkMocker(const std::string &file_directory, const std::string &file_name) : Sink(file_directory, file_name) {}
 };
 
 
 
 TEST(Core, CreateSink) {
-    SinkMocker sink;
+    // Default constructor
+    SinkMocker sink1;
 
-    ASSERT_TRUE(sink._buffer != nullptr);
-    ASSERT_TRUE(sink._buffer_pos == 0);
+    ASSERT_TRUE(sink1._buffer != nullptr);
+    ASSERT_TRUE(sink1.buffer_size() == 0);
+    ASSERT_TRUE(sink1.buffer_max_size() == 500000);
+    ASSERT_TRUE(sink1.file_size() == 0);
+    ASSERT_TRUE(sink1.file_name() == "log.txt");
+    ASSERT_TRUE(sink1.file_directory() == "/home/gabe/Projects/Logger/build/tests/core");
+    ASSERT_TRUE(sink1.file_full_path() == "/home/gabe/Projects/Logger/build/tests/core/log.txt");
+
+    // Directory + log name constructor
+    SinkMocker sink2 ("path/to/logs", "log_file_name.txt");
+
+    ASSERT_TRUE(sink2._buffer != nullptr);
+    ASSERT_TRUE(sink1.buffer_size() == 0);
+    ASSERT_TRUE(sink1.buffer_max_size() == 500000);
+    ASSERT_TRUE(sink1.file_size() == 0);
+    ASSERT_TRUE(sink2.file_name() == "log_file_name.txt");
+    ASSERT_TRUE(sink2.file_directory() == "path/to/logs");
+    ASSERT_TRUE(sink2.file_full_path() == "path/to/logs/log_file_name.txt");
 }
 
 TEST(Core, ShouldFlush) {
@@ -39,10 +58,11 @@ TEST(Core, SinkIn) {
 
     sink.sink_in(message_to_sink);
 
-    ASSERT_TRUE(sink._buffer_pos == message_to_sink.size());
-    ASSERT_TRUE(message_to_sink[sink._buffer_pos - 1] == '!');
+    ASSERT_TRUE(sink.file_size() == 0);
+    ASSERT_TRUE(sink.buffer_size() == message_to_sink.size());
+    ASSERT_TRUE(message_to_sink[sink.buffer_size() - 1] == '!');
 
-    for (int i = 0; i < sink._buffer_pos; i++) {
+    for (int i = 0; i < sink.buffer_size(); i++) {
         ASSERT_TRUE(sink._buffer[i] == message_to_sink[i]);
     }
 }
@@ -50,22 +70,20 @@ TEST(Core, SinkIn) {
 TEST(Core, Flush) {
     SinkMocker sink;
 
-    std::ofstream file ("test_sink_flush.txt", std::ios::out);
     std::string message = "Message to sink! And then flush";
 
     sink.sink_in(message);
-    sink.flush(file);
+    sink.flush();
 
-    ASSERT_TRUE(file.tellp() == message.size());
-    ASSERT_TRUE(sink._buffer_pos == 0);
+    ASSERT_TRUE(sink.file_size() == message.size());
+    ASSERT_TRUE(sink.buffer_size() == 0);
 }
 
-TEST(Core, DestroySink) {
-    SinkMocker sink;
-    sink.~Sink();
+// TEST(Core, DestroySink) {
+//     SinkMocker sink;
 
-    ASSERT_TRUE(sink._buffer == nullptr);
-}
+//     ASSERT_TRUE(sink._buffer == nullptr);
+// }
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);

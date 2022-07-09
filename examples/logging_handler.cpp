e102a4374b43c69f673229431c8246034c61be32
André Gabriel <andremmvgabriel@gmail.com>
2022-07-09 23:31:42 +0100

    Completed logging handler example.

--- examples/logging_handler.cpp
+++ examples/logging_handler.cpp
@@ -1,15 +1,64 @@
 #include <gabe/logging/logging.hpp>
 
+// Typedefs just to agile the accessibility  of methods
 typedef gabe::logging::SeverityLevel SevLvl;
+typedef gabe::logging::LoggerHandler LogHdl;
+
+void add_values_and_log(int val1, int val2) {
+    int result = val1 + val2;
+    LOG(SevLvl::DEBUG, "My addition: {} + {} = {}", val1, val2, result);
+    LOG(SevLvl::DEBUG, "{:^20s} {:^4d} + {:^4d} = {:^4d}", "My addition:", val1, val2, result);
+}
+
+void log_with_copy_handler(LogHdl logger) {
+    logger.trace("Testing {:^10s} logs from a {:^10s} handler.", "TRACE", "COPY");
+    logger.debug("Testing {:^10s} logs from a {:^10s} handler.", "DEBUG", "COPY");
+    logger.info("Testing {:^10s} logs from a {:^10s} handler.", "INFO", "COPY");
+    logger.warning("Testing {:^10s} logs from a {:^10s} handler.", "WARNING", "COPY");
+    logger.error("Testing {:^10s} logs from a {:^10s} handler.", "ERROR", "COPY");
+    logger.fatal("Testing {:^10s} logs from a {:^10s} handler.", "FATAL", "COPY");
+}
+
+void log_with_pointer_handler(LogHdl *logger) {
+    logger->trace("Testing {:^10s} logs from a {:^10s} handler.", "TRACE", "POINTER");
+    logger->debug("Testing {:^10s} logs from a {:^10s} handler.", "DEBUG", "POINTER");
+    logger->info("Testing {:^10s} logs from a {:^10s} handler.", "INFO", "POINTER");
+    logger->warning("Testing {:^10s} logs from a {:^10s} handler.", "WARNING", "POINTER");
+    logger->error("Testing {:^10s} logs from a {:^10s} handler.", "ERROR", "POINTER");
+    logger->fatal("Testing {:^10s} logs from a {:^10s} handler.", "FATAL", "POINTER");
+}
+
+void log_with_reference_handler(LogHdl &logger) {
+    logger.trace("Testing {:^10s} logs from a {:^10s} handler.", "TRACE", "REFERENCE");
+    logger.debug("Testing {:^10s} logs from a {:^10s} handler.", "DEBUG", "REFERENCE");
+    logger.info("Testing {:^10s} logs from a {:^10s} handler.", "INFO", "REFERENCE");
+    logger.warning("Testing {:^10s} logs from a {:^10s} handler.", "WARNING", "REFERENCE");
+    logger.error("Testing {:^10s} logs from a {:^10s} handler.", "ERROR", "REFERENCE");
+    logger.fatal("Testing {:^10s} logs from a {:^10s} handler.", "FATAL", "REFERENCE");
+}
+
+void log_with_handler_recalling() {
+    // Gets the logger with "logging_handler_example" name. (It will be given the one created previously. You don't have to reconfigure again)
+    LogHdl logger = GET_LOGGER("logging_handler_example");
+
+    logger.trace("Testing {:^10s} logs from a {:^10s} handler.", "TRACE", "RECALL");
+    logger.debug("Testing {:^10s} logs from a {:^10s} handler.", "DEBUG", "RECALL");
+    logger.info("Testing {:^10s} logs from a {:^10s} handler.", "INFO", "RECALL");
+    logger.warning("Testing {:^10s} logs from a {:^10s} handler.", "WARNING", "RECALL");
+    logger.error("Testing {:^10s} logs from a {:^10s} handler.", "ERROR", "RECALL");
+    logger.fatal("Testing {:^10s} logs from a {:^10s} handler.", "FATAL", "RECALL");
+}
 
 int main() {
     /*
     Overview:
+        This example should cover how to use the logging handler object.
+
     Notes:
     */
 
-    // 
-    gabe::logging::LoggerHandler logger = GET_LOGGER("logging_handler_example");
+    // Gets the logger with "logging_handler_example" name. (It creates it since it does not exist yet)
+    LogHdl logger = GET_LOGGER("logging_handler_example");
 
     // Quick logger configuration (just to change the default values)
     logger.basic_config(
@@ -18,32 +67,31 @@ int main() {
         "logging_handler_example" // Log file name
     );
 
-    // The previous line is the same as executing the following 3
+    // The previous configuration line is the same as executing the following 3
     logger.set_severity(SevLvl::DEBUG);
     logger.set_logs_location("logs");
     logger.set_logs_file_name("logging_handler_example");
 
-    // Logging using specific
-    logger.trace("Testing TRACE from LOG function.");
-    logger.debug("Testing DEBUG from LOG function.");
-    logger.info("Testing INFO from LOG function.");
-    logger.warning("Testing WARNING from LOG function.");
-    logger.error("Testing ERROR from LOG function.");
-    logger.fatal("Testing FATAL from LOG function.");
+    // Logging using LoggerHandler object
+    logger.trace("Function call: logger.trace().");
+    logger.debug("Function call: logger.debug().");
+    logger.info("Function call: logger.info().");
+    logger.warning("Function call: logger.warning().");
+    logger.error("Function call: logger.error().");
+    logger.fatal("Function call: logger.fatal().");
 
-    // LOG(SevLvl::TRACE, "Testing TRACE from LOG function.");
-    // LOG(SevLvl::DEBUG, "Testing DEBUG from LOG function.");
-    // LOG(SevLvl::INFO, "Testing INFO from LOG function.");
-    // LOG(SevLvl::WARNING, "Testing WARNING from LOG function.");
-    // LOG(SevLvl::ERROR, "Testing ERROR from LOG function.");
-    // LOG(SevLvl::FATAL, "Testing FATAL from LOG function.");
+    logger.log(SevLvl::TRACE, "Function call: logger.log(TRACE)");
+    logger.log(SevLvl::DEBUG, "Function call: logger.log(DEBUG)");
+    logger.log(SevLvl::INFO, "Function call: logger.log(INFO)");
+    logger.log(SevLvl::WARNING, "Function call: logger.log(WARNING)");
+    logger.log(SevLvl::ERROR, "Function call: logger.log(ERROR)");
+    logger.log(SevLvl::FATAL, "Function call: logger.log(FATAL)");
 
-    // TRACE("Testing TRACE from TRACE function.");
-    // DEBUG("Testing DEBUG from DEBUG function.");
-    // INFO("Testing INFO from INFO function.");
-    // WARNING("Testing WARNING from WARNING function.");
-    // ERROR("Testing ERROR from ERROR function.");
-    // FATAL("Testing FATAL from FATAL function.");
+    // Logging within functions
+    log_with_copy_handler(logger);
+    log_with_pointer_handler(&logger);
+    log_with_reference_handler(logger);
+    log_with_handler_recalling();
     
     return 0;
 }

18784964b40d217cfb0240bed6971fd97699373b
André Gabriel <andremmvgabriel@gmail.com>
2022-07-09 16:28:23 +0100

    Finished simple logging example.

--- examples/simple_logging.cpp
+++ examples/simple_logging.cpp
@@ -9,16 +9,19 @@ void add_values_and_log(int val1, int val2) {
     LOG(SevLvl::DEBUG, "{:^20s} {:^4d} + {:^4d} = {:^4d}", "My addition:", val1, val2, result);
 }
 
+
+
 int main() {
     /*
     Overview:
-        This example should cover how easy you can start logging.
-
-        You have have to import the logging header file and you are good to go!
+        This example should cover how easy you can start logging. You just have have to import the logging header file and you are good to go!
 
     Notes:
-        The default logger created in the background is called "main". Also by default, the log file created by it has its name.
+        1) The "SET_DEFAULT_LOGGER", "SET_DEFAULT_LOGS_DIRECTORY", and "SET_DEFAULT_SEVERITY" functions are not mandatory to be used in order to start logging. It is only used to change the default values managed by the internal entity for example purposes.
     */
+
+    // Changes the default logger name from "main" to "simple_logging"
+    SET_DEFAULT_LOGGER("simple_logging");
 
     // Change the default log files location from "root" to "logs"
     SET_DEFAULT_LOGS_DIRECTORY("logs");
@@ -26,6 +29,7 @@ int main() {
     // Change the default log severity from "INFO" to "TRACE"
     SET_DEFAULT_SEVERITY(SevLvl::TRACE);
 
+    // Logging functions
     LOG(SevLvl::TRACE, "Testing TRACE from LOG function.");
     LOG(SevLvl::DEBUG, "Testing DEBUG from LOG function.");
     LOG(SevLvl::INFO, "Testing INFO from LOG function.");
@@ -40,7 +44,8 @@ int main() {
     ERROR("Testing ERROR from ERROR function.");
     FATAL("Testing FATAL from FATAL function.");
 
+    // Logging outside scope
     add_values_and_log(10, 40);
-    
+
     return 0;
 }

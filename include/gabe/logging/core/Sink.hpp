f2559a24f2a6895093a546a712b8225ff2cc57b4
André Gabriel <andremmvgabriel@gmail.com>
2022-07-30 01:39:40 +0100

    Documented Sink object class.

--- include/gabe/logging/core/Sink.hpp
+++ include/gabe/logging/core/Sink.hpp
@@ -9,41 +9,166 @@
 namespace gabe {
     namespace logging {
         namespace core {
+            /**
+             * @brief Sink object.
+             * 
+             * This class is aimed to manage the logging data that was logged into a Logger object and has to be written in a file. This class also manages multiple log files, in case it has to be rotated by a Handler.
+             */
             class Sink
             {
             protected:
+                /**
+                 * @brief Logging file.
+                 */
                 std::ofstream _file;
+
+                /**
+                 * @brief Base name of the logging file.
+                 */
                 std::string _file_name;
+
+                /**
+                 * @brief Directory of the log file.
+                 */
                 std::string _file_directory;
 
+                /**
+                 * @brief Buffer to stash logging data before writting into file. 
+                 */
                 char *_buffer = nullptr;
+
+                /**
+                 * @brief Maximum size of the buffer.
+                 */
                 uint32_t _buffer_size = 500000;
+
+                /**
+                 * @brief Buffer write position.
+                 */
                 uint32_t _buffer_pos = 0;
             
             public:
+                /**
+                 * @brief Construct a new Sink object.
+                 * 
+                 * Default constructor. This method creates a Sink object with the log file as "log.txt", and its directory as a relative path to the program executable. 
+                 */
                 Sink();
+
+                /**
+                 * @brief Construct a new Sink object.
+                 * 
+                 * Full path constructor. This method creates a Sink object with a given log file name and directory.
+                 * 
+                 * @param file_directory Path to the log file.
+                 * @param file_name Log file name.
+                 */
                 Sink(const std::string &file_directory, const std::string &file_name);
+
+                /**
+                 * @brief Destroy the Sink object.
+                 */
                 ~Sink();
 
+                /**
+                 * @brief Opens the log file in write only mode.
+                 * 
+                 * @note If the file is already opened, the function exits immediately without throwing any exception/error.
+                 */
                 void open_file();
+
+                /**
+                 * @brief Closes the opened log file.
+                 * 
+                 * @note If the file is not opened, the function exits immediately without throwing any exception/error.
+                 */
                 void close_file();
 
+                /**
+                 * @brief Adds the message to the sink buffer.
+                 * 
+                 * @param message Message to sink in the buffer.
+                 */
                 void sink_in(const std::string &message);
+
+                /**
+                 * @brief Flushes the data in the buffer into the log file.
+                 * 
+                 * Writes the data stashed in the buffer into the log file. Also restarts the buffer writting position.
+                 */
                 void flush();
+
+                /**
+                 * @brief Analyses if the buffer data should be flushed into the log file.
+                 * 
+                 * @param message Message to sink in. 
+                 * @return bool Should/Shouldn't flush the buffer into the log file.
+                 */
                 bool should_flush(const std::string &message);
 
+                /**
+                 * @brief Renames the log file.
+                 * 
+                 * Once called, this functions makes sure that the log file is properly closed, and then proceeds to rename it with the new given name. This is commonly used by log file Handlers, that the user may want to add to the Logger object.
+                 * 
+                 * @param new_name New name of the log file.
+                 */
                 void rotate_file(const std::string &new_name);
 
+                /**
+                 * @brief Get the current size of the file.
+                 * 
+                 * @return uint32_t File size.
+                 */
                 uint32_t file_size();
+
+                /**
+                 * @brief Get the current size of the buffer.
+                 * 
+                 * @return uint32_t Buffer size.
+                 */
                 uint32_t buffer_size();
+                
+                /**
+                 * @brief Gets the maximum size of the buffer.
+                 * 
+                 * @return uint32_t Buffer maximum size.
+                 */
                 uint32_t buffer_max_size();
 
+                /**
+                 * @brief Set the name of the log file.
+                 * 
+                 * @param name Name of the log file.
+                 */
                 void set_file_name(const std::string &name);
+
+                /**
+                 * @brief Get the name of the log file.
+                 * 
+                 * @return std::string Name of the log file.
+                 */
                 std::string get_file_name();
 
+                /**
+                 * @brief Set the path to the log file.
+                 * 
+                 * @param directory Path to the log file.
+                 */
                 void set_file_directory(const std::string &directory);
+
+                /**
+                 * @brief Get the path to the log file.
+                 * 
+                 * @return std::string Path to the log file.
+                 */
                 std::string get_file_directory();
 
+                /**
+                 * @brief Get the full path to the log file.
+                 * 
+                 * @return std::string Full path to the log file.
+                 */
                 std::string get_file_full_path();
             };
         }
--- src/gabe/logging/core/Sink.cpp
+++ src/gabe/logging/core/Sink.cpp
@@ -3,16 +3,20 @@
 #include <filesystem>
 
 gabe::logging::core::Sink::Sink() : _file_directory(""), _file_name("log.txt") {
+    // Creates the buffer with the desired size
     _buffer = new char[_buffer_size];
 
+    // Opens the log file if it already exists in the system
     if (std::filesystem::exists(get_file_full_path())) {
         open_file();
     }
 }
 
 gabe::logging::core::Sink::Sink(const std::string &file_directory, const std::string &file_name) : _file_directory(file_directory), _file_name(file_name) {
+    // Creates the buffer with the desired size
     _buffer = new char[_buffer_size];
 
+    // Opens the log file if it already exists in the system
     if (std::filesystem::exists(get_file_full_path())) {
         open_file();
     }
@@ -20,13 +24,17 @@ gabe::logging::core::Sink::Sink(const std::string &file_directory, const std::st
 
 gabe::logging::core::Sink::~Sink() {
     close_file();
+
+    // Makes sure the buffer is deleted and assigned to nullptr
     if (_buffer) delete _buffer;
     _buffer = nullptr;
 }
 
 void gabe::logging::core::Sink::open_file() {
+    // Safety check
     if (_file.is_open()) return;
 
+    // Opens the log file in write only mode
     _file = std::ofstream(
         get_file_full_path(),
         std::ios::out | std::ios::app
@@ -34,8 +42,10 @@ void gabe::logging::core::Sink::open_file() {
 }
 
 void gabe::logging::core::Sink::close_file() {
+    // Safety check
     if (!_file.is_open()) return;
 
+    // Closes the file
     _file.close();
 }
 
@@ -43,14 +53,21 @@ void gabe::logging::core::Sink::sink_in(const std::string &message) {
     // Safety check
     if (should_flush(message)) return;
 
+    // Adds the message content into the buffer
     memcpy(&_buffer[_buffer_pos], message.data(), message.size());
 
+    // Updates the buffer writing position pointer
     _buffer_pos += message.size();
 }
 
 void gabe::logging::core::Sink::flush() {
+    // Makes sure the file is opened
     open_file();
+
+    // Writes the buffer data into the log file
     _file.write(_buffer, _buffer_pos);
+
+    // Restarts the buffer writing position pointer
     _buffer_pos = 0;
 }
 
@@ -60,15 +77,19 @@ bool gabe::logging::core::Sink::should_flush(const std::string &message) {
 }
 
 void gabe::logging::core::Sink::rotate_file(const std::string &new_name) {
+    // Makes sure that every data from the buffer is inserted into the log file
     flush();
     close_file();
 
+    // Creates the full path to the new log file
     std::string new_full_path = std::filesystem::path(get_file_directory()) / new_name;
 
+    // Renames it
     std::rename(get_file_full_path().c_str(), new_full_path.c_str());
 }
 
 uint32_t gabe::logging::core::Sink::file_size() {
+    // This has to be done like this. Otherwise the tellp function would return the maximum size of the file.
     if (_file.is_open())
         return _file.tellp();
     else
@@ -84,6 +105,7 @@ uint32_t gabe::logging::core::Sink::buffer_max_size() {
 }
 
 void gabe::logging::core::Sink::set_file_name(const std::string &name) {
+    // Moves the log file is it is already opened (logged into)
     if (_file.is_open()) {
         close_file();
         
@@ -92,7 +114,9 @@ void gabe::logging::core::Sink::set_file_name(const std::string &name) {
         _file_name = name;
         std::string new_name = get_file_full_path();
 
+        // Renames the file (moving it)
         rename(old_name.data(), new_name.data());
+
         open_file();
     } else {
         _file_name = name;
@@ -104,6 +128,7 @@ std::string gabe::logging::core::Sink::get_file_name() {
 }
 
 void gabe::logging::core::Sink::set_file_directory(const std::string &directory) {
+    // Moves the log file is it is already opened (logged into)
     if (_file.is_open()) {
         close_file();
         
@@ -112,7 +137,9 @@ void gabe::logging::core::Sink::set_file_directory(const std::string &directory)
         _file_directory = directory;
         std::string new_name = get_file_full_path();
 
+        // Renames the file (moving it)
         rename(old_name.data(), new_name.data());
+        
         open_file();
     } else {
         _file_directory = directory;
